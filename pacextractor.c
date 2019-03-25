6e070fcb70b35cfc6f98b85904818d8de1c761ec
superr <superr@ddayweb.com>
2019-03-25 13:48:31 -0500

    Added arguments, -f filename, -o outdir. -f is a required argument. -o is an optional argument. -o defaults to the current directory. If -o is specified, create outdir if it does not exist.

--- pacextractor.c
+++ pacextractor.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <sys/stat.h>
+#include <limits.h>
 
 typedef struct {
     int16_t someField[24];
@@ -50,14 +51,40 @@ void getString(int16_t* baseString, char* resString) {
 }
 
 int main(int argc, char** argv) {
-    if(argc < 2) {
-        printf("Usage:  pacextractor <firmware name>.pac\n");
-        exit(EXIT_FAILURE);
-    }
+    char tmp[PATH_MAX];
+    char* outdir = "./";
+    char* filename = NULL;
+
+    extern char *optarg;
+	extern int optind;
+	int c, err = 0;
+	int fflag=0;
+	static char usage[] = "usage: %s -f filename [-o outdir]\n";
+
+	while ((c = getopt(argc, argv, "f:o:")) != -1) switch (c) {
+		case 'f':
+			fflag = 1;
+			filename = optarg;
+			break;
+		case 'o':
+			outdir = optarg;
+			break;
+		case '?':
+			err = 1;
+			break;
+	}
+	if (fflag == 0) {
+		fprintf(stderr, "%s: missing -f option\n", argv[0]);
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	} else if (err) {
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	}
     
-    int fd = open(argv[1], O_RDONLY);
+    int fd = open(filename, O_RDONLY);
     if (fd == -1) {
-        printf("file %s not found\n", argv[1]);
+        printf("file %s not found\n", filename);
         exit(EXIT_FAILURE);
     }
     
@@ -65,15 +92,15 @@ int main(int argc, char** argv) {
 //    int firmwareSize = (fd);
 //    fseek(fd, 0, SEEK_SET);
     struct stat st;
-    stat(argv[1], &st);
+    stat(filename, &st);
     int firmwareSize = st.st_size;
     if(firmwareSize < sizeof(PacHeader)) {
-        printf("file %s is not firmware\n", argv[1]);
+        printf("file %s is not firmware\n", filename);
         exit(EXIT_FAILURE);
     }
     
     PacHeader pacHeader;
-    size_t rb =read(fd, &pacHeader, sizeof(PacHeader));
+    size_t rb = read(fd, &pacHeader, sizeof(PacHeader));
     if(rb <= 0) {
         printf("Error while parsing PAC header\n");
         exit(EXIT_FAILURE);
@@ -89,7 +116,7 @@ int main(int argc, char** argv) {
     for(i = 0; i < pacHeader.partitionCount; i++) {
         lseek(fd, curPos, SEEK_SET);
         uint32_t length;
-        rb =read(fd, &length, sizeof(uint32_t));
+        rb = read(fd, &length, sizeof(uint32_t));
         if(rb <= 0) {
             printf("Partition header error\n");
             exit(EXIT_FAILURE);
@@ -97,7 +124,7 @@ int main(int argc, char** argv) {
         partHeaders[i] = malloc(length);
         lseek(fd, curPos, SEEK_SET);
         curPos += length;
-        rb =read(fd, partHeaders[i], length);
+        rb = read(fd, partHeaders[i], length);
         if(rb <= 0) {
             printf("Partition header error\n");
             exit(EXIT_FAILURE);
@@ -107,6 +134,10 @@ int main(int argc, char** argv) {
         printf("Partition name: %s\n\twith file name: %s\n\twith size %u\n", buffer, buffer1, partHeaders[i]->partitionSize);
     }
     
+    if (stat(outdir, &st) == -1) {
+        mkdir(outdir, 0755);
+    }
+
     for(i = 0; i < pacHeader.partitionCount; i++) {
         if(partHeaders[i]->partitionSize == 0) {
             free(partHeaders[i]);
@@ -115,13 +146,14 @@ int main(int argc, char** argv) {
         lseek(fd, partHeaders[i]->partitionAddrInPac, SEEK_SET);
         getString(partHeaders[i]->fileName, buffer);
         remove(buffer);
-        int fd_new = open(buffer, O_WRONLY | O_CREAT, 0666);
+        sprintf(tmp, "%s/%s", outdir, buffer);
+        int fd_new = open(tmp, O_WRONLY | O_CREAT, 0666);
         printf("Extract %s\n", buffer);
         uint32_t dataSizeLeft = partHeaders[i]->partitionSize;
         while(dataSizeLeft > 0) {
             uint32_t copyLength = (dataSizeLeft > 256) ? 256 : dataSizeLeft;
             dataSizeLeft -= copyLength;
-            rb =read(fd, buffer, copyLength);
+            rb = read(fd, buffer, copyLength);
             if(rb != copyLength) {
                 printf("Partition image extraction error\n");
                 exit(EXIT_FAILURE);
